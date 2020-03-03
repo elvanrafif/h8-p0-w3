54ef2136ed4d036bb3787ad2dc9e659212da241f
elvanrafif <terlanjurbuta@gmail.com>
2020-03-03 19:20:26 +0700

    tugas exercises2 done

---
+++ exercises2.js
@@ -0,0 +1,11 @@
+let str = ['Hello World!']
+let tampung = ''
+
+function balikString(str){
+    for (var i = str[0].length-1; i >= 0; i--){
+        tampung += str[0][i]
+    }
+    return tampung
+}
+
+console.log(balikString(str))
