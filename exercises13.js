2da3de0347b71f59f59bad6c85911cb109a3ff0a
elvanrafif <terlanjurbuta@gmail.com>
2020-03-08 15:22:56 +0700

    ex13 selesai

--- exercises13.js
+++ exercises13.js
@@ -1,6 +1,33 @@
 function targetTerdekat(arr) {
-    // you can only write your code here!
+  var jarak = 0
+  for(var i=0; i<arr.length; i++) {
+    if(arr[i]=='o') {
+      for(var j=0; j<arr.length; j++) {
+        if(arr[j]=='o') {
+          jarak = 0
+        } else if(arr[j]==' ') {
+          jarak ++
+        } else if(arr[j]=='x') {
+          jarak ++
+          return jarak
+        }
+      } 
+      return 0;     
+    } else if(arr[i]=='x') {
+      for(var k=0; k<arr.length; k++) {
+        if(arr[k]=='x') {
+          jarak = 0;
+        } else if(arr[k]==' ') {
+          jarak ++
+        } else if(arr[k]=='o') {
+          jarak ++
+          return jarak
+        }
+      }
+      return 0;
+    }
   }
+}
   
   // TEST CASES
   console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
